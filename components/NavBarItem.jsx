bfd762c215822b57532d0cff741c5607ddfc9513
shyam041 <shyam041@gmail.com>
2025-04-25 13:08:58 +0530

    Add files via upload

---
+++ components/NavBarItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { usePathname } from 'next/navigation';
+
+const NavBarItem = ({ children, href, className, icon, tabIndex, testId }) => {
+  const pathname = usePathname();
+  const activeClass = 'navbar-item-active';
+  const activeClasses = className ? `${className} ${activeClass}` : activeClass;
+
+  return (
+    <span className="d-inline-flex align-items-center navbar-item">
+      {icon && <span>YeahHey</span>}
+      <span className={pathname === href ? activeClasses : className} tabIndex={tabIndex} data-testid={testId}>
+        {children}
+      </span>
+    </span>
+  );
+};
+
+export default NavBarItem;
