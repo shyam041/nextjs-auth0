bc56bfaf1e104a23e7143753607294758326b259
shyam041 <shyam041@gmail.com>
2025-07-11 21:12:09 +0530

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,52 +1,50 @@
+// Force Node.js runtime to support streaming uploads
+export const runtime = "nodejs"
+
 import { auth0 } from "@/lib/auth0"
 import { NextRequest, NextResponse } from "next/server"
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL!
+
 export async function proxyRequest(
     req: NextRequest,
     method: string,
-    pathSegments: string[],
+    pathSegments: string[]
 ): Promise<NextResponse> {
     const url = `${BASE_URL}/${pathSegments.join("/")}${req.nextUrl.search}`
 
-    const headers: HeadersInit = {}
-    req.headers.forEach((value, key) => {
-        console.log("THE HEADERS ", key, "==>", value)
-        headers[key] = value;
-    })
+    // Clone and forward headers
+    const headers: HeadersInit = { ...Object.fromEntries(req.headers.entries()) }
 
-    // Check if request already includes an Authorization header
-    const authHeader = req.headers.get("authorization")
-    if (authHeader) {
-        // console.log("authHeader are present:: ", authHeader)
-        headers["Authorization"] = authHeader
-    } else {
+    // Ensure Authorization header is present
+    if (!headers["authorization"]) {
         const { token } = await auth0.getAccessToken()
-        console.log("NEW TOKEN CREATED ON PROXY :: ", token)
-        if (token) {
-            headers["Authorization"] = `Bearer ${token}`
-        }
-        else {
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
+        if (!token) return new NextResponse("Unauthorized", { status: 401 })
+        headers["Authorization"] = `Bearer ${token}`
     }
+
     const isStreaming = ["POST", "PUT", "PATCH"].includes(method)
+
     const init: RequestInit = {
         method,
         headers,
-        body: isStreaming ? req.body : undefined,
-        ...(isStreaming ? { duplex: "half" as const } : {})
+        body: ["POST", "PUT", "PATCH"].includes(method) ? await req.text() : undefined,
     }
+
     try {
         const response = await fetch(url, init)
+
         if ([204, 304].includes(response.status)) {
             return new NextResponse(null, { status: response.status })
         }
+
         const responseBody = await response.text()
 
         return new NextResponse(responseBody, {
             status: response.status,
             headers: {
-                "Content-Type": response.headers.get("content-type") || "application/json",
+                "Content-Type":
+                    response.headers.get("content-type") || "application/json",
             },
         })
     } catch (err) {
